eb590c73261129c37263a838bda096056f574137
Lasse Eskildsen <les@vertica.dk>
2019-01-29 22:32:52 +0100

    WIP: Styling

--- src/todo-pwa/Client/components/shell/shell.jsx
+++ src/todo-pwa/Client/components/shell/shell.jsx
@@ -1,12 +1,12 @@
 import * as React from "react";
 import { Counter } from "../counter/counter";
 import { TodoList } from "../todo-list/todo-list";
-import "./shell.less";
+//import "./shell.css";
 export class Shell extends React.Component {
     render() {
         return (
             <div>
-                 <p>Shell.jsx</p>
+                 <p>Shell.jsxl</p>
                  {/* <Counter /> */}
                  <TodoList />
             </div>
--- src/todo-pwa/wwwroot/serviceworker.js
+++ src/todo-pwa/wwwroot/serviceworker.js
@@ -6,9 +6,9 @@ var filesToCache = [
 self.addEventListener("install", function(event) {
     console.log("[ServiceWorker] Installing");
     event.waitUntil(
-        caches.open(cacheName).then(function(cache) {
+        caches.open(cacheName).then(function (cache) {
             console.log("[ServiceWorker] Caching files");
-            return cache.addAll(filesToCache)
+            return cache.addAll(filesToCache);
         })
-    )
+    );
 });
